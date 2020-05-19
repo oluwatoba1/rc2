cf9de8ada14a1e533967c114b9c49be7b4bb42cc
toba96 <tohbyy@gmail.com>
2020-05-19 17:08:29 +0100

    Redux

--- src/App.js
+++ src/App.js
@@ -5,7 +5,6 @@ import {
 	BrowserRouter as Router,
 	Redirect
 } from 'react-router-dom';
-import { Provider } from 'react-redux';
 
 import './App.css';
 import Login from './components/auth/Login';
@@ -16,24 +15,21 @@ import Savings from './components/savings/Savings';
 // import AuthState from './context/auth/AuthState';
 // import AlertState from './context/alert/AlertState';
 import Layout from './layout/Layout';
-import store from './store';
 
 const App = () => {
 	return (
-		<Provider store={store}>
-			<Router>
-				<div className="rc-container">
-					<Switch>
-						<Route path="/login" component={Login} />
-						<Route path="/register" component={Register} />
-						<Layout>
-							<PrivateRoute path="/" component={Dashboard} />
-							<PrivateRoute path="/savings" component={Savings} />
-						</Layout>
-					</Switch>
-				</div>
-			</Router>
-		</Provider>
+		<Router>
+			<div className="rc-container">
+				<Switch>
+					<Route path="/login" component={Login} />
+					<Route path="/register" component={Register} />
+					<Layout>
+						<PrivateRoute path="/" component={Dashboard} />
+						<PrivateRoute path="/savings" component={Savings} />
+					</Layout>
+				</Switch>
+			</div>
+		</Router>
 	);
 };
 
