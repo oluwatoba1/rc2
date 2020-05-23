7360e39312ee27adf4a039c8221c821486c02153
toba96 <tohbyy@gmail.com>
2020-05-23 21:38:03 +0100

    Login Design Update

--- src/layout/Layout.js
+++ src/layout/Layout.js
@@ -1,16 +1,48 @@
-import React, {Fragment, useState, useEffect} from 'react';
+import React, { Fragment, useState, useEffect } from 'react';
+import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
 
 import Sidebar from './Sidebar';
 import Header from './Header';
 
 import setAuthToken from '../utils/setAuthToken';
+import { logout } from '../actions/authActions';
 
 if (localStorage.token) setAuthToken(localStorage.token);
 
-const Layout = (props) => {
+const Layout = ({ logout, children }) => {
 	const [showText, setShowText] = useState(false);
+	const [counter, setCounter] = useState(0);
+
+	const events = ['load', 'mousedown', 'click', 'scroll', 'keypress'];
+
+	const resetTimeout = () => {
+		setCounter(0);
+		clearTimeout();
+	};
+
+	for (let i in events) {
+		window.addEventListener(events[i], resetTimeout);
+	}
+
+	const countUp = () => {
+		//	Start count
+		let countdown = setInterval(() => {
+			setCounter(counter => counter + 1);
+		}, 1000);
+	};
+
+	//	logout after a period of inactivity
+	if (counter === 10) {
+		// clearInterval(countdown);
+		for (let i in events) {
+			window.removeEventListener(events[i], resetTimeout);
+		}
+		logout();
+	}
 
 	useEffect(() => {
+		// expand and hide menu bar
 		if (showText) {
 			document.getElementById('sidebar').style.width = '120px';
 			document.getElementById('logoHeader').style.width = '120px';
@@ -18,6 +50,9 @@ const Layout = (props) => {
 			document.getElementById('sidebar').style.width = '60px';
 			document.getElementById('logoHeader').style.width = '60px';
 		}
+		//	count and logout after period of inactivity
+		countUp();
+		//	eslint-disable-next-line
 	}, [showText]);
 
 	const expand = () => {
@@ -30,10 +65,15 @@ const Layout = (props) => {
 				<div className="rightHeader">
 					<Header showText={showText} expand={expand} />
 				</div>
-				<div className="content">{props.children}</div>
+				<p>{counter}</p>
+				<div className="content">{children}</div>
 			</div>
 		</Fragment>
 	);
 };
 
-export default Layout;
+Layout.propTypes = {
+	logout: PropTypes.func
+};
+
+export default connect(null, { logout })(Layout);
